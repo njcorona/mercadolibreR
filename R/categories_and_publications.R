2213974381045406b4a2aa8870b0ccc3a18cdfdc
Nicolas Corona <njcorona@wharton.upenn.edu>
2020-05-16 13:32:50 -0400

    get_site_listing_exposures, get_site_listing_prices

--- R/categories_and_publications.R
+++ R/categories_and_publications.R
@@ -36,3 +36,53 @@ get_site_listing_types <- function(site_id) {
 
     return(as_tibble(res))
 }
+
+#' Get site listing exposures.
+#'
+#' @param site_id Required. site_id of the country of interest.  See mercadolibreR::get_sites() or mercadolibreR::get_site_ids() for valid site_ids.
+#' @return
+#' Returns a data frame (tibble) of listing exposures for the given country's Mercado Libre site.  Fields include id, name, home_page, category_home_page, advertising_on_listing_page, and priority_in_search.  See the API \href{https://developers.mercadolibre.com.co/es_ar/categorias-y-publicaciones}{documentation} for more information.
+#' @export
+#'
+get_site_listing_exposures <- function(site_id) {
+    if (!site_id %in% get_site_ids()) {
+        stop("STOP get_site_listing_exposures: invalid site_id.  See mercadolibreR::get_sites() or mercadolibreR::get_site_ids() for valid site_ids.")
+    }
+
+    url <- str_glue("https://api.mercadolibre.com/sites/", site_id, "/listing_exposures")
+    res <- RETRY('GET', url)
+
+    verify_result(res, "get_site_listing_exposures")
+
+    res <- fromJSON(content(res, as = 'text', encoding = 'UTF-8'), flatten = TRUE)
+
+    return(as_tibble(res))
+}
+
+#' Get site listing prices.
+#'
+#' @param site_id Required. site_id of the country of interest.  See mercadolibreR::get_sites() or mercadolibreR::get_site_ids() for valid site_ids.
+#' @param price Required.  Maximum price for listing and selling.
+#' @return
+#' Returns a data frame (tibble) of prices for various listing types for the given country's Mercado Libre site.  Fields include listing_type_id, listing_type_name, listing_exposure, requires_picture, currency_id, listing_fee_amount, sale_fee_amount, and free_relist.  See the API \href{https://developers.mercadolibre.com.co/es_ar/categorias-y-publicaciones}{documentation} for more information.
+#' @export
+#'
+get_site_listing_prices <- function(site_id, price) {
+    if (!site_id %in% get_site_ids()) {
+        stop("STOP get_site_listing_prices: invalid site_id.  See mercadolibreR::get_sites() or mercadolibreR::get_site_ids() for valid site_ids.")
+    }
+
+    url <- str_glue("https://api.mercadolibre.com/sites/", site_id, "/listing_prices")
+
+    params <- list(
+        price = price
+    )
+
+    res <- RETRY('GET', url, query = params)
+
+    verify_result(res, "get_site_listing_prices")
+
+    res <- fromJSON(content(res, as = 'text', encoding = 'UTF-8'), flatten = TRUE)
+
+    return(as_tibble(res))
+}
--- R/unimplemented.R
+++ R/unimplemented.R
@@ -7,4 +7,7 @@
     # I started trying to implement this in set_user_information in users.R
 # require_mercadopago: https://developers.mercadolibre.com.co/es_ar/producto-consulta-usuarios#Mercado-Pago
 # unrequire_mercadopago: https://developers.mercadolibre.com.co/es_ar/producto-consulta-usuarios#Mercado-Pago
-
+# According to the API, there's a function /site_domains/{Site_domain_url}	Devuelve información sobre el dominio.	GET.
+    # However, I don't know what site_domains are (didn't work with "MLA") and the example for this function refers to a
+    # different function, so I'm going to skip it for now.
+    # Link here: https://developers.mercadolibre.com.co/es_ar/categorias-y-publicaciones
